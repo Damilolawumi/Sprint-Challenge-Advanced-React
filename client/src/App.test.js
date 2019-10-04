465c51e35adc7432786d118101ef1f0b13d5c0e9
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-04 11:38:41 +0100

    added a test for useLocalStorage

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Player from './Player';
+import useLocalStorage from './useLocalStorage';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+it('useLocalStorage Component renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<useLocalStorage />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+
+// jest.mock('axios', () => {
+//   return {
+//     get: () => {
+//       return Promise.resolve({
+//         data: [
+//           { id: 0, name: 'Alex Morgan', country: 'United States', searches: '100' }
+//         ],
+//       });
+//     },
+//   };
+// });
+
+// beforeEach(() => {
+//   rtl.cleanup();
+//   player = rtl.render(<Player player={player} />);
+// });
+// describe('App Component', () => {
+//   it('can render a players list that comes from api', () => {
+//     const elementWithPlayer = player.queryByText(/name/);
+//     expect(elementWithPlayer).toBeInTheDocument();
+//   });
+// });
+
