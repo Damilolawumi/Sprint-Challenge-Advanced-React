36e56349f937c2d0fc3d6d6338ed2b034976cc22
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-04 08:46:19 +0100

    added a method componentDidMount and made an axios call

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      
+      players: [],
+     }
+
+     this.componentDidMount () {
+       Axios.get('http://localhost:5000/api/players')
+       .then((response) => {
+
+        this.setState({
+          players: response.data
+        })
+        console.log (response.data);
+       })
      }
   }
   render() { 
