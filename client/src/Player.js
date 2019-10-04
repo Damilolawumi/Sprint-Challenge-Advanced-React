429c5913bddab0b4b044130aa58d9ac8648a877a
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-04 09:16:27 +0100

    imported useLocalStorage to player.js and used it to display each players information to the browser

--- client/src/Player.js
+++ client/src/Player.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import useLocalSorage from './useLocalStorage';
 
-function Player () {
+function Player (props) {
+
+    const [player, setPlayers] = useLocalSorage('p', props.player)
 
     return (
         <div>
+            <p>{player.id}</p>
+            <p>{player.name}</p>
+            <p>{player.country}</p>
+            <p>{player.searches}</p>
         </div>
     )
 }
